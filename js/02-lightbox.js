b7440e22c58abbd7dbd5d8157beb3a6c67175a5a
Viacheslav <gabon.qwa@gmail.com>
2023-06-22 13:55:29 +0300

    tasks 1 and 2 - done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.addEventListener('click', onOpenCloseOriginalImg);
+
+
+const murkup = galleryItems
+  .map(({preview, original, description}) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image" 
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+      </li>`
+  })
+  .join('');
+
+galleryRef.innerHTML = murkup;
+
+
+function onOpenCloseOriginalImg(event) {
+  const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  event.preventDefault();
+  lightbox.open(event);
+}
